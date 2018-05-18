171d39d88c27a1304c57a39f23c3f2f6bc37f1d6
swiackichris <>
2018-05-18 18:55:28 +0200

    SE04_139

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -24,7 +24,9 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-private:	
+private:
+	UPROPERTY(EditAnywhere, Category = Firing)
+	float LaunchSpeed = 100000.f; // TODO find sensible default
 	// Sets default values for this pawn's properties
 	ATank();
 
