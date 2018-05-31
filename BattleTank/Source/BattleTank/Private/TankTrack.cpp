d7c243be9196040955c7c0bf792cfa1f0dcf2bcb
swiackichris <>
2018-05-31 18:20:31 +0200

    SE04_159 Setup Track Throttles

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+#include "Runtime/CoreUObject/Public/UObject/UObjectBaseUtility.h"
+
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle %f"), *Name, Throttle);
+}
+
+
+
+
+
