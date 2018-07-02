674c761a805a69180aa1a4a820782f4ec4c0c189
swiackichris <>
2018-07-02 16:09:21 +0200

    SE04_202 Finishing Health Bar

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -10,6 +10,11 @@
 #include "Runtime/Engine/Classes/Components/SceneComponent.h"
 
 
+float ATank::GetHealthPercent() const
+{
+	return (float)CurrentHealth / (float)StartingHealth;
+}
+
 // Sets default values
 ATank::ATank()
 {
