79b636846fd47cd51ab8ccbddae8a060147dbb28
swiackichris <>
2018-07-13 02:06:27 +0200

    SE04_220 Implementing GetWheel()

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -6,6 +6,7 @@
 #include "Runtime/Engine/Classes/GameFramework/Actor.h"
 #include "Runtime/Engine/Classes/Components/PrimitiveComponent.h"
 #include "Runtime/CoreUObject/Public/UObject/UObjectBaseUtility.h"
+#include "SpawnPoint.h"
 
 #include "SprungWheel.h"
 
@@ -32,6 +33,24 @@ void UTankTrack::DriveTrack(float CurrentThrottle)
 	}
 }
 
+TArray<ASprungWheel*> UTankTrack::GetWheels() const
+{
+	TArray<ASprungWheel*> ResultArray;
+	TArray<USceneComponent*> Children;
+	GetChildrenComponents(true, Children);
+	for (USceneComponent* Child : Children)
+	{
+		auto SpawnPointChild = Cast<USpawnPoint>(Child);
+		if (!SpawnPointChild) continue;
+
+		AActor* SpawnedChild = SpawnPointChild->GetSpawnedActor();
+		auto SprungWheel = Cast<ASprungWheel>(SpawnedChild);
+		if (!SprungWheel) continue;
+
+		ResultArray.Add(SprungWheel);
+	}
+	return ResultArray;
+}
 
 
 
